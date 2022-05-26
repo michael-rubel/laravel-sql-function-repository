7eccf5d16b51a3695315b6581e318c4923b8351e
Michael Rubél <contact@observer.name>
2022-05-26 14:12:50 +0200

    add interface

--- src/SqlFunctionRepository.php
+++ src/SqlFunctionRepository.php
@@ -4,30 +4,8 @@ declare(strict_types=1);
 
 namespace MichaelRubel\SqlFunctionRepository;
 
-use Illuminate\Support\Facades\DB;
-use MichaelRubel\SqlFunctionRepository\Traits\BuildsSqlFunctions;
-
-class SqlFunctionRepository
+interface SqlFunctionRepository
 {
-    use BuildsSqlFunctions;
-
-    /**
-     * @param string|null $connection
-     * @param string|null $select
-     */
-    public function __construct(
-        public ?string $connection = null,
-        public ?string $select = null,
-    ) {
-        if (is_null($this->connection)) {
-            $this->connection = config('sql-function-repository.connection', 'default');
-        }
-
-        if (is_null($this->select)) {
-            $this->select = config('sql-function-repository.select', 'select * from public');
-        }
-    }
-
     /**
      * Execute the database function.
      *
@@ -36,11 +14,5 @@ class SqlFunctionRepository
      *
      * @return array
      */
-    public function runDatabaseFunction(string $name, array $parameters): array
-    {
-        return DB::connection($this->connection)->select(
-            $this->buildSqlFunction($name, $parameters),
-            $parameters
-        );
-    }
+    public function runDatabaseFunction(string $name, array $parameters = []): array;
 }
