7a0971d1766f51ae3f3366bb504a8b865b15ef3f
Michael Rubél <contact@observer.name>
2022-05-26 14:13:34 +0200

    add basic test

--- tests/SqlFunctionRepositoryTest.php
+++ tests/SqlFunctionRepositoryTest.php
@@ -2,11 +2,25 @@
 
 namespace MichaelRubel\SqlFunctionRepository\Tests;
 
+use Illuminate\Database\QueryException;
+use MichaelRubel\SqlFunctionRepository\SqlFunctionRepository;
+use MichaelRubel\SqlFunctionRepository\Traits\ResolvesDatabaseDriver;
+
 class SqlFunctionRepositoryTest extends TestCase
 {
+    use ResolvesDatabaseDriver;
+
     /** @test */
-    public function testCanRunSqlFunction()
+    public function testPostgresqlRepositoryReturnsExceptionOnSqlite()
     {
+        if ($this->usingSQLite()) {
+            $this->expectException(QueryException::class);
+
+            $repository = app(SqlFunctionRepository::class);
+
+            $repository->runDatabaseFunction('testFunctionName', ['value']);
+        }
+
         $this->assertTrue(true);
     }
 }
